2564fc0dbaba785e1f7f157ddbbfad99a95ed0f6
Keerah <9025818+keerah@users.noreply.github.com>
2018-11-06 21:37:34 +0300

    various updates and 2 new presets

---
+++ to h264 medium 420p baseline level 3.0 mp4.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+ECHO [---------------------------------------------------------------------------------]
+ECHO [---  SendTo FFmpeg encoder v1.03 by Keerah.com                                ---]
+ECHO [---  MP4 h264 module has been invoked                                         ---]
+ECHO [---  Preset: 420 baseline 3.0, slow, crf 24, keyfr 2 sec, Audio AAC 128       ---]
+IF %1.==. (
+	ECHO [---------------------------------------------------------------------------------]
+	ECHO [     NO FILE SPECIFIED                                                           ]
+) ELSE (
+	ECHO [---------------------------------------------------------------------------------]
+	ECHO [     Transcoding...                                                              ]
+	"c:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %1 -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p -preset slow -crf 24 -force_key_frames 0:00:02 -c:a aac -b:a 128k -y %~n1_420_constrL3.mp4
+)
+ECHO [---------------------------------------------------------------------------------]
+ECHO [     SERVED                                                                      ]
+ECHO [---------------------------------------------------------------------------------]
+PAUSE
+
+rem Do not forget to replace the path to FFmpeg if its installed into a different folder in your system.
+
+rem This batch encodes to a very strict (very compatible) Baseline v3.0 profile 
+rem It also reencodes the audio to 128 kbps AAC to guarantee full compatibility to virtually any player
+
+rem PAUSE command in the end keeps the batch window open after it's finished just to let you see the messages.
+rem If you need more info on encoding then change verbose level -v command from -v warning to -v info.
---
+++ to h264 very high grain 420p scale1080p mp4.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+ECHO [---------------------------------------------------------------------------------------]
+ECHO [---  SendTo FFmpeg encoder v1.03 by Keerah.com                                      ---]
+ECHO [---  MP4 h264 module has been invoked                                               ---]
+ECHO [---  Preset: 420 main 4.0, veryslow, crf 18, 1080p, GRAIN, keyfr 2 sec Audio Copy   ---]
+IF %1.==. (
+	ECHO [---------------------------------------------------------------------------------------]
+	ECHO [     NO FILE SPECIFIED                                                                 ]
+) ELSE (
+	ECHO [---------------------------------------------------------------------------------------]
+	ECHO [     Transcoding...                                                                    ]
+	"c:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %1 -c:v libx264 -profile:v main -level 4.0 -preset veryslow -crf 18 -pix_fmt yuv420p -vf scale=1920:1080 -sws_flags bicubic -tune grain -force_key_frames 0:00:02 -c:a copy -y %~n1_420_highest_1080p.mp4
+)
+ECHO [---------------------------------------------------------------------------------------]
+ECHO [---  SERVED                                                                            ]
+ECHO [---------------------------------------------------------------------------------------]
+PAUSE
+
+rem Do not forget to replace the path to FFmpeg if its installed into a different folder in your system.
+
+rem PAUSE command in the end keeps the batch window open after it's finished just to let you see the messages.
+rem If you need more info on encoding then change verbose level -v command from -v warning to -v info.
+
+rem ==== this is a dynamic preset I use to encode things differently
