ac4e6f1919b6f45fff3c43ce99ee9fe099e3f570
keerah <9025818+keerah@users.noreply.github.com>
2022-06-19 04:17:40 -0400

    PNG and JPG conversion added
    Added a few presets for quick conversion into images or into a sequence is the source is a video.
    
    Other tiny updates, nearing v 2.3

--- sendtoffmpeg_settings.cmd
+++ sendtoffmpeg_settings.cmd
@@ -1,15 +1,19 @@
 REM SendTo_FFmpeg is a set of windows batches for effortless and free transcoding
-REM Copyright (c) 2018-2021 Keerah, keerah.com. All rights reserved
+REM Copyright (c) 2018-2022 Keerah, keerah.com. All rights reserved
 REM More information at https://keerah.com https://github.com/keerah/SendTo_FFmpeg
 
-REM SendTo_FFmpeg settings file
+REM This is a SendTo_FFmpeg settings file
 REM You need to be careful here, cause it's an executable part of the script for each preset
-REM Multiple files are not implemented for mux (external audio) scripts
+
+REM If you copy of this file to the folder of your source files, then these localized settings
+REM will be used for transcoding all files inside this folder instead of the global settings
+REM For example, you can disable the pause after processing or reduce verbose level to errors only
+REM for a particular folder only. 
 
 rem Path to ffmpeg.exe
 set "ffpath=c:\Program Files\ffmpeg\bin\"
 
-rem FFmpeg verbosity level, use "info" for full verbosity
+rem FFmpeg verbosity level, use "info" for full verbosity or "error" for errors only 
 set "vbl=warning" 
 
 rem Enable pause after encoding, any positive value enables the pause
---
+++ to PNG low.bat
@@ -0,0 +1,59 @@
+@ECHO OFF
+REM SendTo_FFmpeg is a set of windows batches for effortless and free transcoding
+REM Copyright (c) 2018-2021 Keerah, keerah.com. All rights reserved
+REM More information at https://keerah.com https://github.com/keerah/SendTo_FFmpeg
+
+setlocal enabledelayedexpansion
+
+set argCount=0
+for %%x in (%*) do (
+   set /A argCount+=1
+   set "argVec[!argCount!]=%%~x"
+   set "argVn[!argCount!]=%%~nx"
+)
+
+ECHO [---------------------------------------------------------------------------------]
+ECHO [---  SendTo FFmpeg encoder v2.3 by Keerah.com                                 ---]
+ECHO [---  Multi PNG module has been invoked                                        ---]
+ECHO [---  Preset: PNG, compression 9                                               ---]
+
+set "cmdp=%~dp0"
+set "argp=%~dp1"
+
+IF EXIST "%argp%sendtoffmpeg_settings.cmd" ( 
+	CALL "%argp%sendtoffmpeg_settings.cmd"
+	ECHO [---  Settings: LOCAL                                                          ---]
+) ELSE (
+	CALL "%cmdp%sendtoffmpeg_settings.cmd"
+	ECHO [---  Settings: GLOBAL                                                         ---]
+)
+
+IF %argCount% == 0 (
+
+	ECHO [---------------------------------------------------------------------------------]
+	ECHO [     NO FILE SPECIFIED                                                           ]
+	GOTO End
+)
+	
+IF %argCount% GTR 1 (
+
+	ECHO [---------------------------------------------------------------------------------]
+	ECHO [     %argCount% files queued to encode
+)
+
+IF %dscr% GTR 0 (SET "dscrName=_pnglow") ELSE (SET "dscrName=") 
+
+FOR /L %%i IN (1,1,%argCount%) DO (
+	
+	ECHO [---------------------------------------------------------------------------------]
+	ECHO [     Converting %%i of %argCount%: !argVn[%%i]!
+
+	"%ffpath%ffmpeg.exe" -v %vbl% -hide_banner -i "!argVec[%%i]!" -compression_level 9 -y "!argVn[%%i]!%dscrName%.png"
+)
+
+:End
+ECHO [---------------------------------------------------------------------------------]
+ECHO [     SERVED                                                                      ]
+ECHO [---------------------------------------------------------------------------------]
+
+if %pse% GTR 0 PAUSE
